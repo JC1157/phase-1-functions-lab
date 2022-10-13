0b84aff5b267d62d837bf2e966eb37f89ab7a49c
Juan Baez <juan.baez2093@gmail.com>
2022-10-13 17:18:04 -0400

    commit tree 6bced4ca7875d448822f8cbe9c87e25fd7ff013e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const scuberHeadquarters = 42; 
+
+function distanceFromHqInBlocks(ending) {
+    if (ending < scuberHeadquarters) {
+        return scuberHeadquarters - ending 
+    } else 
+        return ending - scuberHeadquarters;
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet (feet) {
+    const result= distanceFromHqInBlocks(feet) * 264;
+    return result 
+}
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, destination) {
+    if ( start > destination ) {
+        const difference = (start - destination) * 264
+        return difference;
+    
+} else if (destination > start) {
+    const difference2 = (destination - start) * 264
+        return difference2;
+} 
+}
+
